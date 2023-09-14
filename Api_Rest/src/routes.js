094529f0c3e8b795d8b5b0e989d4e2e821775123
Pablo Álvarez <palvarezs169@gmail.com>
2023-09-14 12:10:00 -0300

    add initial purchase endpoint

--- Api_Rest/src/routes.js
+++ Api_Rest/src/routes.js
@@ -1,8 +1,11 @@
 const Router = require('koa-router');
 const broker = require('./routes/broker');
+const purchase = require('./routes/purchase');
 
 const router = new Router();
 
 router.use('/stocks', broker.routes());
+
+router.use('/purchase', purchase.routes());
 
 module.exports = router;
--- Api_Rest/src/routes/purchase.js
+++ Api_Rest/src/routes/purchase.js
@@ -1,12 +1,38 @@
 const Router = require('koa-router');
+const { getGeolocation } = require('../helpers/geolocation');
 const router = new Router();
 
 // TODO: Endpoint to create a purchase
-// eslint-disable-next-line require-await
-router.post('purchase.create', '/', async (ctx) => {
+router.post('purchase', '/', async (ctx) => {
   try {
     console.log(ctx.request.body);
+    const geoLocationData = await getGeolocation(ctx.request.body.ip);
+    console.log(geoLocationData);
+    const country = geoLocationData.country;
+    const city = geoLocationData.city;
+    const loc = geoLocationData.loc;
+
+    // Datos a insertar en BDD
+    const purchaseData = {
+      user_id: ctx.request.body.user_id,
+      symbol: ctx.request.body.symbol,
+      group_id: ctx.request.body.group_id,
+      amount: ctx.request.body.amount,
+      country: country,
+      city: city,
+      location: loc,
+    };
+    console.log(purchaseData);
+
+    // TODO: Interact with DB here. Hay que crear el modelo de Purchase
+
+    ctx.body = { message: 'Compra creada con éxito' };
+    ctx.status = 201;
   } catch (error) {
-    console.error(error);
+    console.error('Error en la ruta POST:', error);
+    ctx.throw = 500;
+    ctx.body = { error: error.message };
   }
 });
+
+module.exports = router;
