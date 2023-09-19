e505b8d5c08c0a82632eee99c6af9d24c2c9b806
colobos <colobos@uc.cl>
2023-09-19 02:11:53 -0300

    Model Purcharse Creado

--- Api_Rest/src/routes/purchase.js
+++ Api_Rest/src/routes/purchase.js
@@ -2,6 +2,36 @@ const Router = require('koa-router');
 const { getGeolocation } = require('../helpers/geolocation');
 const router = new Router();
 
+router.get('purchase.show', '/perfildata', async (ctx) => {
+	try {
+    //Obtener el id de la sección...Auth0
+    // const session = await ctx.orm.sessions.findByPk(ctx.session.sessionid);
+    // const userid = session.userid;
+
+	  const historial = await ctx.orm.Purchase.findAll({
+		attributes: [
+		  ['user_id', 'user_id'], 
+		  ['amount', 'amount'], 
+		  ['group_id', 'group_id'], 
+		  ['datetime', 'datetime'], 
+		  ['stocks_symbol', 'symbol'], 
+		  ['stocks_shortname', 'shortName'],
+      ['country', 'country'], 
+      ['city', 'city'], 
+      ['location', 'location']
+		]//,
+    //where: {
+    //  user_id: userid
+    //}
+	  });
+	  ctx.body = historial;
+	} catch (error) {
+	  console.log(error);
+	  ctx.throw(404);
+	}
+  });
+
+
 // TODO: Endpoint to create a purchase
 router.post('purchase', '/', async (ctx) => {
   try {
@@ -15,17 +45,30 @@ router.post('purchase', '/', async (ctx) => {
     // Datos a insertar en BDD
     const purchaseData = {
       user_id: ctx.request.body.user_id,
-      symbol: ctx.request.body.symbol,
-      group_id: ctx.request.body.group_id,
       amount: ctx.request.body.amount,
-      date: ctx.request.body.date,
+      group_id: ctx.request.body.group_id,
+      datetime: ctx.request.body.datetime,
+      stocks_symbol: DataTypes.STRING,
+      stocks_shortname: DataTypes.STRING,
       country: country,
       city: city,
       location: loc,
     };
-    console.log(purchaseData);
 
-    // TODO: Interact with DB here. Hay que crear el modelo de Purchase
+    const purchase = await ctx.orm.Purchase.create({
+      user_id: ctx.request.body.user_id,
+      amount: ctx.request.body.amount,
+      group_id: ctx.request.body.group_id,
+      datetime: ctx.request.body.datetime,
+      stocks_symbol: DataTypes.STRING,
+      stocks_shortname: DataTypes.STRING,
+      country: country,
+      city: city,
+      location: loc,
+    });		
+
+    console.log(purchaseData);
+    console.log(purchase);
 
     ctx.body = { message: 'Compra creada con éxito' };
     ctx.status = 201;
--- DB_App/sql-commands.sql
+++ DB_App/sql-commands.sql
@@ -2,4 +2,6 @@
 
 CREATE TABLE Brokers (id serial PRIMARY KEY, stocks_id varchar(255), datetime varchar(255), stocks_symbol varchar(255), stocks_shortName varchar(255), stocks_price float, stocks_currency varchar(255), stocks_source varchar(255), created_at DATE, updated_at DATE);
 
+CREATE TABLE Purchase (id serial PRIMARY KEY, user_id int, amount int, group_id varchar(255), datetime varchar(255), stocks_symbol varchar(255), stocks_shortName varchar(255), country varchar(255), city varchar(255), location varchar(255), created_at DATE, updated_at DATE);
+
 */
