b1d52c9e286e74be221978bb2c5771ac3f4d7b75
Pablo Álvarez <palvarezs169@gmail.com>
2023-09-21 21:50:31 -0300

    add wallet endpoints and model

--- Api_Rest/src/routes.js
+++ Api_Rest/src/routes.js
@@ -1,11 +1,14 @@
 const Router = require('koa-router');
 const broker = require('./routes/broker');
 const purchase = require('./routes/purchase');
+const wallet = require('./routes/wallet');
 
 const router = new Router();
 
 router.use('/stocks', broker.routes());
 
 router.use('/purchase', purchase.routes());
+
+router.use('/wallet', wallet.routes());
 
 module.exports = router;
---
+++ Api_Rest/src/routes/wallet.js
@@ -0,0 +1,86 @@
+const Router = require('koa-router');
+const router = new Router();
+
+// Get all Wallets
+router.get('wallet.show', '/', async (ctx) => {
+  try {
+    const wallets = await ctx.orm.Wallet.findAll({
+      attributes: [
+        ['user_id', 'userId'], 
+        ['money', 'money']
+      ]
+    });
+    ctx.body = wallets;
+  } catch (error) {
+    console.log(error);
+    ctx.throw(404);
+  }
+});
+
+// Get a Wallet by userId
+router.get('wallet.show', '/:userId', async (ctx) => {
+  try {
+    const wallets = await ctx.orm.Wallet.findAll({
+      attributes: [
+        ['user_id', 'userId'], 
+        ['money', 'money']
+      ],
+      where: {
+        user_id: ctx.params.userId
+      }
+    });
+    ctx.body = wallets;
+  } catch (error) {
+    console.log(error);
+    ctx.throw(404);
+  }
+});
+
+// Create a Wallet
+router.post('wallet.create', '/:userId', async (ctx) => {
+  try {
+    const wallet = await ctx.orm.Wallet.create({
+      user_id: ctx.params.userId,
+      money: 0
+    });
+    if (wallet) {
+      console.log('Wallet data:', wallet);
+    }
+    ctx.body = { message: 'Wallet successfuly created' };
+    ctx.status = 201;
+  } catch (error) {
+    console.error('Error en la ruta POST:', error);
+    ctx.throw = 500;
+    ctx.body = { error: error.message };
+  }
+});
+
+// Update a Wallet
+router.patch('wallet.update', '/:userId', async (ctx) => {
+  try {
+    const added_money = ctx.request.body.money;
+    console.log('Added money:', added_money)
+    const wallet = await ctx.orm.Wallet.findOne({
+      where: {
+        user_id: ctx.params.userId
+      }
+    });
+    if (wallet) {
+      console.log('Wallet data:', wallet);
+      const new_money = wallet.money + added_money;
+      await wallet.update({
+        money: new_money
+      });
+      ctx.body = { message: 'Wallet successfuly updated' };
+    }
+  } catch (error) {
+    console.error('Error en la ruta PATCH:', error);
+    ctx.throw = 500;
+    ctx.body = { error: error.message };
+  }
+});
+
+
+
+
+module.exports = router;
