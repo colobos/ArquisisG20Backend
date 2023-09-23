9826d5d5892e92e6328817603c6539d65abe7e8f
colobos <colobos@uc.cl>
2023-09-23 19:30:35 -0300

    Actualizacion Validacion Compra

--- Api_Rest/src/routes.js
+++ Api_Rest/src/routes.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const broker = require('./routes/broker');
 const purchase = require('./routes/purchase');
 const wallet = require('./routes/wallet');
+const validation = require('./routes/validation');
 
 const router = new Router();
 
@@ -10,5 +11,7 @@ router.use('/stocks', broker.routes());
 router.use('/purchase', purchase.routes());
 
 router.use('/wallet', wallet.routes());
+
+router.use('/validation', validation.routes());
 
 module.exports = router;
--- Api_Rest/src/routes/purchase.js
+++ Api_Rest/src/routes/purchase.js
@@ -1,6 +1,9 @@
+const axios = require('axios');
 const Router = require('koa-router');
 const { getGeolocation } = require('../helpers/geolocation');
 const router = new Router();
+const uuid = require('uuid');
+
 
 router.get('purchase.show', '/perfildata/:userId', async (ctx) => {
   try {
@@ -32,27 +35,64 @@ router.get('purchase.show', '/perfildata/:userId', async (ctx) => {
 
 router.post('purchase', '/', async (ctx) => {
   try {
-    const geoLocationData = await getGeolocation(ctx.request.body.ip);
-    const country = geoLocationData.country;
-    const city = geoLocationData.city;
-    const loc = geoLocationData.loc;
-    const purchase = await ctx.orm.Purchase.create({
-      user_id: ctx.request.body.user_id,
-      amount: ctx.request.body.amount,
-      group_id: ctx.request.body.group_id,
-      datetime: ctx.request.body.datetime,
-      stocks_symbol: ctx.request.body.symbol,
-      stocks_shortname: ctx.request.body.shortname,
-      country: country,
-      city: city,
-      location: loc,
+
+    const requestId = uuid.v4();
+    const bodytosendMqtt = {
+      "request_id": requestId,
+      "group_id": "20",
+      "symbol": ctx.request.body.symbol,
+      "datetime": new Date().toISOString(),
+      "deposit_token": "",
+      "quantity": parseFloat(ctx.request.body.amount),
+      "seller": 0
+    };
+
+    const url = `http://app_listener:8000/request` 
+    //console.log(url)
+    const responseMqtt = await axios.post(url, bodytosendMqtt)
+    //console.log(responseMqtt.data, "response.data")
+
+    const validation = await ctx.orm.Validation.findOne({
+      attributes: [
+        ['request_id', 'request_id'], 
+        ['group_id', 'group_id'], 
+        ['seller', 'seller'], 
+        ['valid', 'valid'],
+      ],
+      where: {
+        request_id: requestId
+      }
     });
-    if (purchase) {
-      console.log('Purchase data:', purchase);
-    }
-    ctx.body = { message: 'Compra creada con éxito', 
-  validate: true};
-    ctx.status = 201;
+
+    if (validation) {
+      if (validation.valid == true)
+        {
+          const geoLocationData = await getGeolocation(ctx.request.body.ip);
+          const country = geoLocationData.country;
+          const city = geoLocationData.city;
+          const loc = geoLocationData.loc;
+          const purchase = await ctx.orm.Purchase.create({
+            user_id: ctx.request.body.user_id,
+            amount: ctx.request.body.amount,
+            group_id: ctx.request.body.group_id,
+            datetime: ctx.request.body.datetime,
+            stocks_symbol: ctx.request.body.symbol,
+            stocks_shortname: ctx.request.body.shortname,
+            country: country,
+            city: city,
+            location: loc,
+          });
+      
+          if (purchase) {
+            console.log('Purchase data:', purchase);
+          }
+          ctx.body = { message: 'Compra creada con éxito', validate: true};
+        }
+        else {
+          ctx.body = { message: 'Compra no logró ser realizada', validate: false};
+        }
+      ctx.status = 201;
+      }
   } catch (error) {
     console.error('Error en la ruta POST:', error);
     ctx.throw = 500;
