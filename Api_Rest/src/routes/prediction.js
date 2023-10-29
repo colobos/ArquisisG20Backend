bb110ce6ad6e425b0bea3148b07fc1935cc026c4
colobos <colobos@uc.cl>
2023-10-29 17:59:56 -0300

    FixPredictionRequest

--- Api_Rest/src/routes/prediction.js
+++ Api_Rest/src/routes/prediction.js
@@ -31,4 +31,35 @@ router.get('prediction.show', '/:userId', async (ctx) => {
 });
 
 
+router.post('prediction.show', '/request', async (ctx) => {
+  try {
+    const formattedData = ctx.request.body;
+    console.log("Información");
+
+    /* 
+    Ejemplo de lo que entrega formattedData:
+      {
+        user_id: '653db29818d81877fbb0590d',
+        symbol: 'AMZN',
+        group_id: '72daf6d3-a30e-4dcb-97a9-922d8800cc6c',
+        amount: '777',
+        time: '777',
+        shortname: 'Amazon.com, Inc.'
+      }
+
+    */
+
+   /*Enviar información a Worker*/
+
+
+    console.log(formattedData);
+    ctx.body = { message: 'Predicción enviada exitosamente' };
+
+  } catch (error) {
+    console.log(error);
+    ctx.throw(404);
+  }
+});
+
+
 module.exports = router;
